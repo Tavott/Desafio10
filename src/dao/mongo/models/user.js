cdb2650dad10aacd11eb6c883cba25722e713ddb
Tavot <guerreroagustavo@hotmail.com>
2023-08-15 20:18:08 -0300

    Entrega10

---
+++ src/dao/mongo/models/user.js
@@ -0,0 +1,38 @@
+import { Schema, model } from "mongoose";
+
+const userCollection = 'users'
+
+const userSchema = Schema({
+    first_name:{
+        type: String,
+        required: true
+    },
+    last_name:{
+        type: String,
+        required: true
+    },
+    age:{
+        type: Number,
+        required: true
+    },
+    roll:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    cart: {
+        type: Schema.Types.ObjectId,
+        ref: 'carts',
+        require: true
+    },
+    password:{
+        type: String,
+        required: true
+    }
+})
+
+export default model(userCollection, userSchema)
